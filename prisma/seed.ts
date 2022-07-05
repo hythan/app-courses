e8c2a2d8e536d717088de27dec33b75ec2d971f1
Hythan <hythan97@gmail.com>
2022-07-05 13:39:51 -0300

    Added seed for initiate the database with one admin

---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+import { BcryptService } from '../src/helpers/bcrypt';
+
+const prisma = new PrismaClient();
+const bcrypt = new BcryptService();
+
+async function main() {
+    const pass = await bcrypt.encrypt('admin');
+    await prisma.admins.create({
+        data: {
+            name: 'admin',
+            email: 'admin@admin.com',
+            password: pass
+        }
+    });   
+}
+
+main().catch(e => {
+    console.log(e);
+    process.exit(1);
+}).finally(() => {
+    prisma.$disconnect();
+})
